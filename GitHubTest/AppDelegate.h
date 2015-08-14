ac68c93846ed692e4e2a71d34f68cc48240942e8
Volker Funke <volker.funke@brainbow.de>
2015-08-14 19:57:57 +0200

    Initial Commit

---
+++ GitHubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubTest
+//
+//  Created by Volker Funke on 14.08.15.
+//  Copyright (c) 2015 Volker Funke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHubTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubTest
+//
+//  Created by Volker Funke on 14.08.15.
+//  Copyright (c) 2015 Volker Funke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
