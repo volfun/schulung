b900f234fd718ab1b99234a92666f6cf83b3143b
Volker Funke <volker.funke@brainbow.de>
2015-08-14 20:32:21 +0200

    Second Change

--- GitHubTest/ViewController.m
+++ GitHubTest/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog( @"First Change");
+    NSLog( @"Second Change");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
